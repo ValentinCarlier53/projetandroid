874ddddb8f537d777482f35ffc3e34a73f5176a8
Valentin Carlier <v.carlier610@laposte.net>
2021-09-29 15:06:06 +0200

    Ajout du navcontroller

---
+++ Android_app/app/src/main/java/fr/eseo/carlieva/android_app/FragmentChoixResp.java
@@ -0,0 +1,53 @@
+package fr.eseo.carlieva.android_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentChoixResp#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentChoixResp extends Fragment implements View.OnClickListener {
+    public FragmentChoixResp() {
+    }
+
+    private View root;
+    private Button buttonScrumMaster;
+    private Button buttonTeamMember;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_choix_resp, container, false);
+        buttonScrumMaster = root.findViewById(R.id.buttonScrumMaster);
+        buttonScrumMaster.setOnClickListener(this);
+        buttonTeamMember = root.findViewById(R.id.buttonTeamMember);
+        buttonTeamMember.setOnClickListener(this);
+
+        // Inflate the layout for this fragment
+        return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        MainActivity main = (MainActivity) getActivity();
+        switch (v.getId()) {
+            case R.id.buttonScrumMaster:
+                main.displayScreen(IdScreen.FRAGMENT_SCRUM_MASTER_MENU);
+                break;
+            case R.id.buttonTeamMember:
+                main.displayScreen(IdScreen.FRAGMENT_TEAM_MEMBER_MENU);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
--- Android_app/app/src/main/java/fr/eseo/carlieva/android_app/MainActivity.java
+++ Android_app/app/src/main/java/fr/eseo/carlieva/android_app/MainActivity.java
@@ -4,16 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     NavController navController;
+    private ProgressDialog progress;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    static MainActivity instance;
+    /**
+     * Used to return the instance of MainActivity
+     *
+     * @return instance
+     */
+    public static MainActivity getInstance() {
+        return instance;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instance = this;
         navController = Navigation.findNavController(this, R.id.fragment_nav_host);
+        progress = new ProgressDialog(MainActivity.this);
 
+
+        preferences = getApplicationContext().getSharedPreferences(getString(R.string.ParamètreVisio), 0);
+        editor = preferences.edit();
     }
+
+    public void displayScreen(IdScreen idScreen) {
+        switch (idScreen) {
+            case FRAGMENT_SCRUM_MASTER_MENU:
+                //actionBar.setDisplayHomeAsUpEnabled(true);
+                navController.navigate(R.id.action_fragmentChoixResp_to_ScrumMasterMenu);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
