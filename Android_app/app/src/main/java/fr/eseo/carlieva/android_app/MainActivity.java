1192b8581dae324af1c05daacba2a2b90c780c46
Valentin Carlier <v.carlier610@laposte.net>
2021-09-29 13:44:24 +0200

    Ajout des vues et de navigation

--- Android_app/app/src/main/java/fr/eseo/carlieva/android_app/MainActivity.java
+++ Android_app/app/src/main/java/fr/eseo/carlieva/android_app/MainActivity.java
@@ -1,14 +1,19 @@
 package fr.eseo.carlieva.android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navController = Navigation.findNavController(this, R.id.fragment_nav_host);
+
     }
 }
