85dce87f638e356fbd93c27d5b278f6e1bbc26c9
Valentin Carlier <v.carlier610@laposte.net>
2021-09-29 15:13:06 +0200

    Revert "Ajout du navcontroller"
    This reverts commit 874ddddb8f537d777482f35ffc3e34a73f5176a8.

--- Android_app/app/src/main/java/fr/eseo/carlieva/android_app/IdScreen.java
+++
@@ -1,7 +0,0 @@
-package fr.eseo.carlieva.android_app;
-/**
- * Enumeration of Screens in app
- */
-public enum IdScreen {
-    FRAGMENT_CHOIX_RESP,FRAGMENT_SCRUM_MASTER_MENU,FRAGMENT_TEAM_MEMBER_MENU
-}
