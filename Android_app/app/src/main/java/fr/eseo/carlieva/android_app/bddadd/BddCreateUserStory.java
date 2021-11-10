a968a3eb8f3fe4e2efb679873dee9f0e09ba746f
lehenaje <jb7507pvb@gmail.com>
2021-11-10 17:59:26 +0100

    Update BddCreateUserStory.java

--- Android_app/app/src/main/java/fr/eseo/carlieva/android_app/bddadd/BddCreateUserStory.java
+++ Android_app/app/src/main/java/fr/eseo/carlieva/android_app/bddadd/BddCreateUserStory.java
@@ -1,4 +1,39 @@
 package fr.eseo.carlieva.android_app.bddadd;
 
-public class BddCreateUserStory {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import fr.eseo.carlieva.android_app.R;
+
+public class BddCreateUserStory extends AppCompatActivity {
+    private static final String TAG = "DocSnippets";
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_scrum_master_menu);
+        Map<String, Object> team = new HashMap<>();
+
+        team.put("Nom", "md");
+        team.put("Votes", FieldValue.arrayUnion());
+        team.put("Note", 0);
+
+        db.collection("UserStory")
+                .add(team);
+
+
+
+    }
+
 }
