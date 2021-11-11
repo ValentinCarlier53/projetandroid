8aa2de37a92b268ae66cacf3c4928c5d126563cf
Valentin Carlier <v.carlier610@laposte.net>
2021-11-11 09:53:33 +0100

    add lancement vote

--- Android_app/app/src/main/java/fr/eseo/carlieva/android_app/fragments/FragmentLancerVote.java
+++ Android_app/app/src/main/java/fr/eseo/carlieva/android_app/fragments/FragmentLancerVote.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import fr.eseo.carlieva.android_app.R;
 
@@ -20,11 +24,42 @@ public class FragmentLancerVote extends Fragment {
     public FragmentLancerVote() {
         // Required empty public constructor
     }
+    ListView listUserStoryVote;
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lancer_vote, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_user_stories, container, false);
+
+        String [] userStoryItems = {"lancer vote us 1", "lancer vote us 2","lancer vote us 3"};
+        listUserStoryVote=(ListView) root.findViewById(R.id.ListUserStory);
+
+        ArrayAdapter<String> listViewAdapter= new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                userStoryItems
+        );
+
+        listUserStoryVote.setAdapter(listViewAdapter);
+        // Log.d(TAG,menuItems[0]);
+        listUserStoryVote.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                MainActivity main = (MainActivity) getActivity();
+                if (position == 0) {
+                    Toast.makeText(getActivity(), "lancement vote us1", Toast.LENGTH_SHORT).show();
+                    //to do mettre le bouleen du lancement su vote à true ;
+                } else if (position == 1) {
+                    Toast.makeText(getActivity(), "lancement vote us2", Toast.LENGTH_SHORT).show();
+                    //to do mettre le bouleen du lancement su vote à true ;
+                }
+            }
+        });
+        return root;
     }
 }
